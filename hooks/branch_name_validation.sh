47fbd1ca6c5b3ec6aef2b58f24c2f70a3f7748de
Kyle Gagnon <kmgagnon99@gmail.com>
2024-07-26 15:23:04 +0000

    ci(all): adding new ci features

---
+++ hooks/branch_name_validation.sh
@@ -0,0 +1,23 @@
+#!/usr/bin
+
+# The following branch patterns are allowed
+#
+# feat/*
+# feature/*
+# fix/*
+# chore/*
+# refactor/*
+# docs/*
+# test/*
+# style/*
+branch_pattern='^(feat|feature|fix|chore|refactor|docs|style|test)\/.+$'
+
+# Get the current branch name
+current_branch=$(git symbolic-ref --short HEAD)
+
+# Check if the branch
+if ! [[ $current_branch =~ $branch_pattern ]]; then
+  echo "Error: Branch name '$current_branch' does not match the required pattern."
+  echo "Please view the CONTRIBUTIONS.md for branch naming conventions."
+  exit 1
+fi
